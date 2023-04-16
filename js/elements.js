21410184259d4311465b2aae58c8fd165eba4915
Gustavo Zonta <gustavozontauk@gmail.com>
2023-04-16 08:04:54 +0100

    :tada: Initial Commit

---
+++ js/elements.js
@@ -0,0 +1,19 @@
+const buttonPlay = document.querySelector(".play")
+const buttonPause = document.querySelector(".pause")
+const buttonStop = document.querySelector(".stop")
+const buttonSet = document.querySelector(".set")
+const buttonSoundOn = document.querySelector(".soundsOn")
+const buttonSoundOff = document.querySelector(".soundsOff")
+const minutesDisplay = document.querySelector(".minutes")
+const secondsDisplay = document.querySelector(".seconds")
+
+export {
+  buttonPlay,
+  buttonPause,  
+  buttonStop,
+  buttonSet,
+  buttonSoundOn,
+  buttonSoundOff,
+  minutesDisplay,
+  secondsDisplay,
+}
